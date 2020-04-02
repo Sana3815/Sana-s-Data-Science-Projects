89cd54ed78a07231dd21852aef64d7e736b97bbd
Sana3815 <63069681+Sana3815@users.noreply.github.com>
2020-04-02 14:47:53 -0400

    Add files via upload

---
+++ Capstone_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+# This notebook is intended to be used for the capstone project section of the IBM Data Science Professional Certificate Specialization course on Coursera.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
